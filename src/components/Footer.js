d0df8eedb81691a4e07daf5ad78b137947f12177
Gustavo Pacchi <h.gustavo.pv777@gmail.com>
2017-08-18 17:34:45 -0400

    first commit -list notes

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => ( < div className = "fooer" >
+    <
+    h > mostrar: < /h> <
+    a href = "" > Todos < /a> {''} <
+    a href = "" > Activos < /a> {''}
+
+    <
+    /div>
+)
+export default Footer
