8c3cf17df9aa4eaa34e19aa7a72b99befc4ae8a5
Gustavo Pacchi <h.gustavo.pv777@gmail.com>
2017-08-18 18:02:57 -0400

    change div class contain and console log

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
 class TodoList extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div className = "contenido">
-        <ul>
-          {
-            this.props.todos.map(todo => <li key={todo.id}>{todo.name}</li>)
-          }
-          </ul>
-        </div>
-        
-    )
-  }
-}
-export default TodoList
+    render() {
+        console.log("hola mundo");
+        return ( <
+            div className = "contenido react" >
+            <
+            ul > {
+                this.props.todos.map(todo => < li key = { todo.id } > { todo.name } < /li>)
+                } <
+                /ul> < /
+                div >
+
+            )
+        }
+    }
+    export default TodoList
